77cf59cf1bd0d39e729a68afd1f8041c25ef80c7
RoninKimura <angelnavarrete1210@gmail.com>
2024-12-16 20:52:28 -0600

    Termino lo que inicie
    Me terminaron jaja

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+function respuesta1() {
+    const mensaje1="Quizas no pueda darte lo mejor del mundo pero ten por seguro que tendras lo mejor de mi";
+    const resultado=document.getElementById('resultado');
+    resultado.innerText=mensaje1;
+}
+function respuesta2() {
+    const mensaje2="Tu indiferencia es la sangre que corre por mis venas";
+    const resultado=document.getElementById('resultado');
+    resultado.innerText=mensaje2;
+}
